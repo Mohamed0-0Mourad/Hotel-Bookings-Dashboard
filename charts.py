0ecbbb7231a7582dd929276c02c885bfca8db057
Mohamed Mourad <mohamedmoradmagdy1000@gmail.com>
2025-01-22 21:58:29 +0200

    a liitle optimiaztion to data transforming

--- charts.py
+++ charts.py
@@ -6,10 +6,11 @@ df = pd.read_csv("hotel_bookings.csv")
 
 def month_numeric(month, to:bool = True):
     con = {"January":1, "February":2, "March":3, "April":4, "May":5, "June":6, "July":7, "August":8, "September":9, "October":10, "November":11, "December":12}
-    rev = dict(zip(con.values(), con.keys()))
     if(to):
         return con[month]
-    else: return rev[month]
+    else: 
+        rev = dict(zip(con.values(), con.keys()))
+        return rev[month]
 
 canc = df[df['is_canceled'] == 1]
 def trend_line(year:int):
